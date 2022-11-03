e697b3c4d953112ceea3792fba6d99d7022f3461
Qaiser Mahmood <stepinnsolution@gmail.com>
2022-11-03 18:38:30 +0500

    first changes

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,87 +1,105 @@
 <template>
   <div class="pb-5">
     <div class="m-auto text-center">
-        <img src="../assets/settings.png" alt="Paris" class="m-auto">
-        <h4 class="pt-3">Settings</h4>
-        <hr class="hr-login col-4 m-auto"/>
+      <img src="../assets/settings.png" alt="Paris" class="m-auto">
+      <h4 class="pt-3">Settings</h4>
+      <hr class="hr-login col-4 m-auto" />
     </div>
   </div>
   <div class="row">
     <div class="col-md text-center">
-        <h5>Accont Information</h5>
-        <hr class="hr-login"/>
-        <div>
-            <p>Name: Cooper Moelchert</p>
-            <p>Email: cooper@strange-ventures.com</p>
-            <p>Phone#: (999)999-9999</p>
-            
-        </div>
+      <div style="display: inline-block">
+        <h5>Account Information</h5>
+        <hr class="hr-login" />
+      </div>
+      <div>
+        <p>Name: {{ loading ? '' : user.name }}</p>
+        <p>Email: {{ loading ? '' : user.email }}</p>
+        <p>Phone#: {{ loading ? '' : user.phone }}</p>
+      </div>
     </div>
-    <div class="col-md text-end">				
-        <form class="form-group m-auto">
-            <div class=" text-end">
-                <div class="form-group py-1">
-                    <label for="Student">Enter Old Password:</label>
-                    <input 
-                        type="password" 
-                        class="form-control-pass mx-3"/>
-                </div>
-                <div class="form-group py-1">
-                    <label for="Student">Enter New Password:</label>
-                    <input 
-                        type="password" 
-                        class="form-control-pass mx-3"/>
-                </div>
-                <div class="form-group py-1">
-                    <label for="Student">Retype New Password:</label>
-                    <input 
-                        type="password" 
-                        class="form-control-pass mx-3"/>
-                </div>
-            </div>
-            <div class="text-end">
-                <router-link to="/forgot-password"><p>I forgot my password</p></router-link>
-                <button class="btn mx-3 col-4 shadow" type="submit" 
-                style="background-color: #C3F9D1; color: black; border-radius: 20px;">
-                    <p style="margin-bottom: 0;">Save</p>
-                </button>
-            </div>
+    <div class="col-md text-end">
+      <form class="form-group m-auto">
+        <div class=" text-end">
+          <div class="form-group py-1">
+            <label for="Student">Enter Old Password:</label>
+            <input type="password" class="form-control-pass mx-3" />
+          </div>
+          <div class="form-group py-1">
+            <label for="Student">Enter New Password:</label>
+            <input type="password" class="form-control-pass mx-3" />
+          </div>
+          <div class="form-group py-1">
+            <label for="Student">Confirm New Password:</label>
+            <input type="password" class="form-control-pass mx-3" />
+          </div>
+        </div>
+        <div class="text-end">
+          <router-link to="/forgot-password">
+            <p>I forgot my password</p>
+          </router-link>
+          <button class="btn mx-3 col-4 shadow" type="submit"
+            style="background-color: #C3F9D1; color: black; border-radius: 20px;">
+            <p style="margin-bottom: 0;">Save</p>
+          </button>
+        </div>
 
-        </form>
+      </form>
     </div>
   </div>
   <div class="d-flex justify-content-center align-items-center">
     <div class="text-center">
-        <h4>Regulatory Information</h4>
-        <hr class="hr-login" />
-        <p>Privacy Policy</p>
-        <p>Terms of Service</p>
+      <h4>Regulatory Information</h4>
+      <hr class="hr-login" />
+      <a href="https://docs.google.com/document/d/1_" target="_blank">Privacy Policy</a><br />
+      <a href="https://docs.google.com/document/d/" target="_blank">Terms of Service</a>
     </div>
   </div>
 </template>
 
+<script>
+import useAuth from "@/composables/useAuth";
+
+export default {
+  setup() {
+    const { user, loading, getAuthUser } = useAuth();
+    getAuthUser();
+
+    return {
+      loading,
+      user
+    }
+  }
+}
+</script>
+
 <style>
-.card-bg{
-	background-color: #0D47AA;
+.card-bg {
+  background-color: #0D47AA;
   color: white;
   border-radius: 10px;
 }
-.border-nav{
+
+.border-nav {
   border-bottom: 5px solid black;
 }
-.border-bar{
+
+.border-bar {
   border-bottom: 3px solid black;
 }
-.border-ver{
+
+.border-ver {
   border-right: 5px solid black;
   padding: 0;
 }
-.hr-login{
-	padding: 2px ;
-	background-color: black !important;
+
+.hr-login {
+  padding: 2px;
+  background-color: black !important;
 }
-.form-control-pass{
-	border-radius: 10px !important;
-	border: black solid;
+
+.form-control-pass {
+  border-radius: 10px !important;
+  border: black solid;
 }
 </style>
